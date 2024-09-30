e84918487acf1b3faf81f816f494ed36d18b5160
McNamara <holgerehrmann@gmail.com>
2024-09-30 18:27:18 +0000

    fixed missing port for database

--- database.py
+++ database.py
@@ -12,7 +12,7 @@ def get_db_connection():
     try:
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST'),
-            port=int(os.getenv('DB_PORT')),
+            port=int(os.getenv('DB_PORT', '3306')),
             database=os.getenv('DB_NAME'),
             user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD')
