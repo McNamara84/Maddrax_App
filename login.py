40db99f688e3cde2ddace5fbde7b8aaeff11ab28
McNamara <holgerehrmann@gmail.com>
2024-09-30 17:34:05 +0000

    fixed login

--- login.py
+++ login.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import database as db
 
+
 def login():
     st.header("Login")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    
+
     if st.button("Login"):
         user = db.get_user(username, password)
         if user:
             st.session_state.user = user
             st.success("Logged in successfully!")
-            st.experimental_rerun()
+            st.rerun()  # Hier wurde die Änderung vorgenommen
         else:
             st.error("Invalid username or password")
